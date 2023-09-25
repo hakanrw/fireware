11666ea698c0a9861bf3489ec9431e19882a47f3
hakan <hakancandar@protonmail.com>
2023-09-25 18:02:16 +0300

    add new weapons and update sprites

--- network/shop_controller.gd
+++ network/shop_controller.gd
@@ -27,16 +27,16 @@ class Item:
 var shop = [
 	# rifle
 	## Item.new(0 , "7.62 guerilla", 205, Utils.WeaponType.PRIMARY),
-	Item.new(1 , "cv47", 2700, Utils.WeaponType.PRIMARY, {"cooldown": 200, "damage": 20, "ammo": 15, "mag": 45}),
+	Item.new(1 , "cv47", 270, Utils.WeaponType.PRIMARY, {"cooldown": 200, "damage": 20, "ammo": 15, "mag": 45}),
 	## Item.new(2 , "maverick m4a1", 3100, Utils.WeaponType.PRIMARY),
 	# sidearm
 	Item.new(5 , "7/24 sidearm", 400, Utils.WeaponType.SECONDARY, {"cooldown": 500, "damage": 25, "ammo": 8, "mag": 24}),
-	## Item.new(6 , "wrestler", 500, Utils.WeaponType.SECONDARY),	
+	Item.new(6 , "wrestler", 500, Utils.WeaponType.SECONDARY),	
 	# smg
-	## Item.new(10, "fastgun", 1050, Utils.WeaponType.PRIMARY),
+	Item.new(10, "fastgun", 1050, Utils.WeaponType.PRIMARY, {"cooldown": 200, "damage": 10, "ammo": 16, "mag": 48}),
 	# heavy
 	##Item.new(15, "sawn off", 1100, Utils.WeaponType.PRIMARY),
-	Item.new(16, ".51 blaster", 3000, Utils.WeaponType.PRIMARY, {"cooldown": 750, "damage": 50, "ammo": 2, "mag": 6}),	
+	Item.new(16, ".51 blaster", 300, Utils.WeaponType.PRIMARY, {"cooldown": 750, "damage": 50, "ammo": 2, "mag": 6}),	
 	# misc
 	# Item.new(20, "kevlar vest", 650, Utils.WeaponType.WEAR),
 	Item.new(21, "x grenade", 300, Utils.WeaponType.MISC, {"cooldown": 500}),
