d5a9749181cbdda4bed20c3e17c385c959b6772f
hakan <hakancandar@protonmail.com>
2023-09-28 14:21:36 +0300

    add website button

--- ui/menu/menu.gd
+++ ui/menu/menu.gd
@@ -8,6 +8,7 @@ var menus = {
 
 onready var pane = get_tree().get_nodes_in_group("pane")[0]
 onready var screen = pane.get_node("MarginContainer/VBoxContainer/Screen")
+onready var website_button = $MarginContainer/HBoxContainer/Control/BWebsite
 
 func _ready():
 	var bottom_container = get_tree().get_nodes_in_group("bottom_container")[0]
@@ -15,6 +16,7 @@ func _ready():
 	bottom_container.get_node("BSettings").connect("mouse_clicked", self, "_change_pane_page", ["Settings"])
 	bottom_container.get_node("BExit").connect("mouse_clicked", self, "_exit_pressed")
 	bottom_container.get_node("BCreateServer").connect("mouse_clicked", self, "_create_server")
+	website_button.connect("mouse_clicked", self, "_launch_website")
 	
 	pane.get_node("Button").connect("mouse_clicked", self, "_close_pane")
 
@@ -22,6 +24,7 @@ func _exit_pressed():
 	get_tree().quit()
 
 func _change_pane_page(page):
+	website_button.visible = false
 	pane.visible = true
 	pane.get_node("MarginContainer/VBoxContainer/Title").text = page
 	var screen = pane.get_node("MarginContainer/VBoxContainer/Screen")
@@ -31,9 +34,12 @@ func _change_pane_page(page):
 	print(page)
 
 func _close_pane():
+	website_button.visible = true
 	pane.visible = false
 	
 func _create_server():
 	print(OS.get_cmdline_args())
 	OS.execute(OS.get_executable_path(), ["--server"], false)
-	pass
+
+func _launch_website():
+	OS.shell_open("https://hakan.candar.dev")
